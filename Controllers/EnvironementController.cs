5a65f564696229fe88759459674a5c9e884ac7f5
AbdennourHsn <119530347+AbdennourHsn@users.noreply.github.com>
2024-01-24 12:40:07 +0100

    Update EnvironementController.cs

--- Controllers/EnvironementController.cs
+++ Controllers/EnvironementController.cs
@@ -13,6 +13,11 @@ namespace api.Controllers
             _context = context;
         }
 
+        [HttpGet]
+        public ActionResult<IEnumerable<Entities.Environment>> GetEnvironments()
+        {
+            return _context.Environments.ToList();
+        }
         [HttpGet("{id}")]
         public ActionResult<Entities.Environment> GetEnvironmentById(int id)
         {
@@ -21,17 +26,55 @@ namespace api.Controllers
             else return Ok(env);
         }
 
+        [HttpPost]
+        public ActionResult<Entities.Environment> CreateEnvironment(Entities.Environment environment)
+        {
+            _context.Environments.Add(environment);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(GetEnvironmentById), new { id = environment.Id }, environment);
+        }
+
         [HttpDelete("{id}")]
-        public void DeleteEnvironment(int id)
+        public ActionResult<Entities.Environment> DeleteEnvironment(int id)
         {
             var environmentToDelete = _context.Environments.Find(id);
 
-            if (environmentToDelete != null)
+            if (environmentToDelete == null)
             {
-                _context.Environments.Remove(environmentToDelete);
-                _context.SaveChanges();
+                return NotFound();
             }
+
+            _context.Environments.Remove(environmentToDelete);
+            _context.SaveChanges();
+
+            return environmentToDelete;
         }
+        [HttpPut("{id}")]
+        public IActionResult UpdateEnvironment(int id, Entities.Environment updatedEnvironment)
+        {
+            if (id != updatedEnvironment.Id)
+            {
+                return BadRequest();
+            }
+
+            var existingEnvironment = _context.Environments.Find(id);
+
+            if (existingEnvironment == null)
+            {
+                return NotFound();
+            }
+
+            existingEnvironment.name = updatedEnvironment.name;
+            existingEnvironment.dateOfCreation = updatedEnvironment.dateOfCreation;
+            existingEnvironment.NbrOfCars = updatedEnvironment.NbrOfCars;
+
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+
 
 
     }
