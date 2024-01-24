01f916e61ff5af5a51e7cced181148e2a02d3aa8
AbdennourHsn <119530347+AbdennourHsn@users.noreply.github.com>
2024-01-24 12:35:24 +0100

    Envirenement

--- Controllers/EnvironementController.cs
+++ Controllers/EnvironementController.cs
@@ -1,5 +1,7 @@
 
 using api.Data;
+using api.DTOs;
+using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
 {
@@ -11,6 +13,26 @@ namespace api.Controllers
             _context = context;
         }
 
-        
+        [HttpGet("{id}")]
+        public ActionResult<Entities.Environment> GetEnvironmentById(int id)
+        {
+            var env = this._context.Environments.FirstOrDefault(e => e.Id == id);
+            if (env == null) return NotFound("Envirenement Not founded");
+            else return Ok(env);
+        }
+
+        [HttpDelete("{id}")]
+        public void DeleteEnvironment(int id)
+        {
+            var environmentToDelete = _context.Environments.Find(id);
+
+            if (environmentToDelete != null)
+            {
+                _context.Environments.Remove(environmentToDelete);
+                _context.SaveChanges();
+            }
+        }
+
+
     }
 }
